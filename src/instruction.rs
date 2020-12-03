f0f8791f3390b128cb756efc9b40525566b486e5
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-12-03 20:06:56 +0100

    add check whether message length is as specified in message

--- src/instruction.rs
+++ src/instruction.rs
@@ -20,15 +20,20 @@ impl ZoolanaInstruction {
             0 => {
                 let (sender, rest) = rest.split_first().ok_or(InvalidInstructionData)?;
                 let (buffer_id, rest) = rest.split_first().ok_or(InvalidInstructionData)?;
+                let message_length = rest
+                    .get(..2)
+                    .and_then(|slice| slice.try_into().ok())
+                    .map(u16::from_le_bytes)
+                    .ok_or(InvalidInstructionData)?;
+                let message = (*rest.get(2..).ok_or(InvalidInstructionData)?).to_vec();
+                if message.len() > 65535 || message_length != message.len() as u16 {
+                    return Err(InvalidInstructionData);
+                }
                 Self::WriteMessage {
                     sender: *sender,
                     buffer_id: *buffer_id,
-                    message_length: rest
-                        .get(..2)
-                        .and_then(|slice| slice.try_into().ok())
-                        .map(u16::from_le_bytes)
-                        .ok_or(InvalidInstructionData)?,
-                    message: (*rest.get(2..).ok_or(InvalidInstructionData)?).to_vec(),
+                    message_length,
+                    message,
                 }
             }
             1 => Self::CloseConnection,
