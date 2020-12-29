21d95b789d3732af587cbcf3bc88b7a107b207a2
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-12-29 02:08:07 +0100

    clippy and fmt

--- src/processor.rs
+++ src/processor.rs
@@ -12,7 +12,7 @@ pub struct Processor {}
 
 impl Processor {
     pub fn process(
-        program_id: &Pubkey,
+        _program_id: &Pubkey,
         accounts: &[AccountInfo],
         instruction_data: &[u8],
     ) -> ProgramResult {
